7654cb26c19b6d9eee44137cb92697bf633314e9
Bruce LeCaptain <brulecap@gmail.com>
2018-02-22 10:03:23 -0500

    Added feeling lucky radio button to registration assignment.

--- angular/registration/src/app/user.ts
+++ angular/registration/src/app/user.ts
@@ -5,6 +5,7 @@ export class User {
 		public last_name: string = "",
 		public email: string = "",
 		public password: string = "",
+		public lucky: string= "",
 		public created_at: Date = new Date(),
 		public updated_at: Date = new Date()
 	){}
